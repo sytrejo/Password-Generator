62366afcfb1628c0c2dfad4f16a23c2f24a95c61
Silvia Trejo <s.y.trejo26@gmail.com>
2021-11-13 22:22:42 -0500

    The finished product

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-document.querySelector("#generate").addEventListener("click", generatePassword);
+document.querySelector("#generate").addEventListener("click", writePassword);
 
 var confirmLength = "";
 var confirmNumber;
@@ -11,75 +11,58 @@ number = [0,1,2,3,4,5,6,7,8,9];
 symbols = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]"];
 upperCase = ["A","B","C","D","E","F","G", "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-space = [];
 
 function generatePassword(){
-  var confirmLength = parseInt(prompt("Input the number of characters for your password? min: 8 max: 128"));
-  while (!confirmLength || (confirmLength < 8 || confirmLength > 128)){
-    alert("Please, enter a valid number of characters between 8 and 128.");
-    var confirmLength = parseInt(prompt("Input the number of characters for your password? min: 8 max: 128"));
-  } if (confirmLength && confirmLength >= 8 && confirmLength <= 128)
-    alert ("Your password will have " + confirmLength + " characters.");
+  var confirmLength = parseInt(prompt("Input the number of characters in password? min: 8 max: 128"));
+
+    while(!confirmLength || (confirmLength < 8 || confirmLength > 128)){
+      alert("Please, enter a valid number of characters between 8 and 128.");
+      var confirmLength = parseInt(prompt("Input the number of characters for your password? min: 8 max: 128"));
+    } alert ("Your password will have " + confirmLength + " characters.");
+
     var confirmNumber = confirm ("Would you like your password to include numbers?");
     var confirmSymbols = confirm ("Would you like your password to include symbols?");
     var confirmUpperCase = confirm ("Would you like your password to include Upper Case letters?");
     var confirmLowerCase = confirm ("Would you like your password to include Lower Case letters?");
-      if ((confirmUpperCase === false) && (confirmNumber === false)  && (confirmSymbols === false)  && (confirmLowerCase === false))
-        alert ("Please select a valid criteria for your password!");
-        // var confirmNumber = confirm ("Would you like your password to include numbers?");
-        // var confirmSymbols = confirm ("Would you like your password to include symbols?");
-        // var confirmUpperCase = confirm ("Would you like your password to include Upper Case letters?");
-        // var confirmLowerCase = confirm ("Would you like your password to include Lower Case letters?");}
-  
-var passwordText = []
 
-if(confirmLowerCase){
-  passwordText = passwordText.concat(lowerCase)
-}
-if(confirmUpperCase){
-  passwordText = passwordText.concat(upperCase)
-}
-if(confirmSymbols){
-  passwordText = passwordText.concat(symbols)
-}
-if(confirmNumber){
-  passwordText = passwordText.concat(number)
-}
+      while ((confirmUpperCase === false) && (confirmNumber === false)  && (confirmSymbols === false)  && (confirmLowerCase === false)){
+        alert ("Please select a valid criteria for your password!");
+        var confirmNumber = confirm ("Would you like your password to include numbers?");
+        var confirmSymbols = confirm ("Would you like your password to include symbols?");
+        var confirmUpperCase = confirm ("Would you like your password to include Upper Case letters?");
+        var confirmLowerCase = confirm ("Would you like your password to include Lower Case letters?");
+      }
+
+      var passwordText = []
+
+    if(confirmLowerCase){
+      passwordText = passwordText.concat(lowerCase)
+    }
+    if(confirmUpperCase){
+      passwordText = passwordText.concat(upperCase)
+    }
+    if(confirmSymbols){
+      passwordText = passwordText.concat(symbols)
+    }
+    if(confirmNumber){
+      passwordText = passwordText.concat(number)
+    }
 
 console.log (passwordText)
 
-//   } else if (confirmSymbols && confirmUpperCase && confirmLowerCase && confirmNumber){
-//       selections = symbols.concat(number, lowerCase, upperCase);
-//   } else if (confirmSymbols && confirmUpperCase && confirmLowerCase){
-//     selections = symbols.concat(lowerCase, upperCase);
-//   } else if (confirmSymbols && confirmUpperCase){
-//     selections = symbols.concat(upperCase);
-//   }
-// }
-
-// var password = [];
-// for (var i = 0; i < length; i++){
-//   var pickedSelections = selections[Math.floor(Math.random())];
-//   password.push(pickedSelections);
-// }
+var randomPassword = ""
+      
+for (var i = 0; i < confirmLength; i++) {
+  randomPassword = randomPassword + passwordText[Math.floor(Math.random() * passwordText.length)];
+  console.log(randomPassword)
+}
+return randomPassword;
+}
 
 
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//  passwordText.value = password;
-// }
-
-// generateBtn.addEventListener("click", writePassword);
-
-// var selections;
-
-
-// var generateBtn = document.querySelector("#generate");
-
-// generateBtn.addEventListener("click",function (){
-//   ps = generatePassword();
-//   document.getElementById("password").placeholder = "Your password goes here!";
-// })
-
+passwordText.value = password;
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password-Generator
+I established the variables to be confirmed by the user
+
+Then I created an array for each variable to define the characters that would be available for the passwords. I know that the symbols could have more but I decided to limit the characters.
+
+Aftter, I created the function that would generate the password. I created a while loop to ensure that the user selects a number of characters between 8 and 128. I decided to add an alert to notify the user if they selected the correct number of characters. I also included a confirmation alert to notify the user the length of the password. 
+ 
+
+The if loops concat the variable into the password if the user elects to include the variable.
+
+
